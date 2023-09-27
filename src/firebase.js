fb04f1b3aa171ba78b35d7385a8e86e07df3952e
Isah Ibrahim <isahibn08@gmail.com>
2023-09-27 16:20:16 +0100

    remove firebase

--- src/firebase.js
+++ src/firebase.js
@@ -3,14 +3,7 @@ import { getAuth } from 'firebase/auth';
 import { getStorage } from 'firebase/storage';
 import { getFirestore } from 'firebase/firestore';
 
-const firebaseConfig = {
-	apiKey: 'AIzaSyB6fvbbKPsLSyZrUYTB1EXWqLrc3RKt7cY',
-	authDomain: 'chat-app-85c3d.firebaseapp.com',
-	projectId: 'chat-app-85c3d',
-	storageBucket: 'chat-app-85c3d.appspot.com',
-	messagingSenderId: '591103313822',
-	appId: '1:591103313822:web:e49146b0dca9b639b8788f',
-};
+const firebaseConfig = {};
 
 // Initialize Firebase
 export const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
